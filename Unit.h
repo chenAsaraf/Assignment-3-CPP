edc548f2009cef92c4aab495b514af108d7870f8
chenAsaraf <44566578+chenAsaraf@users.noreply.github.com>
2019-04-02 14:42:20 +0300

    initial code

---
+++ Unit.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+namespace ariel{
+    
+    enum class Unit{
+        //Length
+        KM, //Kilometer=0 By deafult
+        M, //Meter=1
+        CM, //=2
+        //Time
+        SEC,
+        MIN,
+        HOUR,
+        //Weight
+        G,
+        KG,
+        TON
+    };
+    
+    
+}
