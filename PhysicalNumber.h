7643468cb5596bc4823b5a560edaaf77d702234c
chenAsaraf <44566578+chenAsaraf@users.noreply.github.com>
2019-04-02 14:42:47 +0300

    initial code

---
+++ PhysicalNumber.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include "Unit.h"
+
+namespace ariel{
+    class PhysicalNumber{
+        enum Unit;
+        int amount
+        int dimension; //maby not neccesary
+        
+        //Constructors:
+        PhysicalNumber(int amount, Unit unit){
+            
+        }
+        
+        
+    }//end of PhysicalNumber
+    
+}//end of namespace ariel
